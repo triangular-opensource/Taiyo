935273546c0c6baaf23abae17f5400a57cf54d98
Alankar Saxena <saxenaalankar42@gmail.com>
2021-11-26 22:54:18 +0530

    Initial Commit

--- Taiyo/asgi.py
+++ Taiyo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for apexMarketing project.
+ASGI config for Taiyo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apexMarketing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taiyo.settings')
 
 application = get_asgi_application()
--- Taiyo/urls.py
+++ Taiyo/urls.py
@@ -1,4 +1,4 @@
-"""apexMarketing URL Configuration
+"""Taiyo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
