a15394c477b8270554908646853de2943005eb99
Alankar Saxena <saxenaalankar42@gmail.com>
2021-11-27 14:30:31 +0530

    admin and global views completed

--- TaiyoInfo/views.py
+++ TaiyoInfo/views.py
@@ -4,12 +4,12 @@ from TaiyoInfo.serializers import GeneralInfoSerializer
 
 from services.response import success_response, bad_request_response, empty_response, create_response, not_found_response
 
-from TaiyoInfo.models import GenralInfo
+from TaiyoInfo.models import GeneralInfo
 
 
-class OrganizationView(generics.RetrieveAPIView):
+class GeneralInfoView(generics.RetrieveAPIView):
     serializer_class = GeneralInfoSerializer
 
     def get(self, request, *args, **kwargs):
-        serializer = self.get_serializer(GenralInfo.objects.all())
+        serializer = self.get_serializer(GeneralInfo.objects.all(), many=True)
         return success_response(serializer.data)
