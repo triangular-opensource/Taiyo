d3e5631342da86f44dede3273670dc5ae9647710
savagecarol <karthiksharma1411@gmail.com>
2021-11-30 16:39:40 +0530

    all api of news letter policy and general info address

--- TaiyoInfo/serializers.py
+++ TaiyoInfo/serializers.py
@@ -7,3 +7,19 @@ class GeneralInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = GeneralInfo
         fields = "__all__"
+
+class NewsLetterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GeneralInfo
+        fields = "__all__"
+
+class PolicySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GeneralInfo
+        fields = "__all__"
+
+class AddresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GeneralInfo
+        fields = "__all__"
+
--- TaiyoInfo/urls.py
+++ TaiyoInfo/urls.py
@@ -4,4 +4,7 @@ from TaiyoInfo.views import GeneralInfoView
 
 urlpatterns = [
     path("general-info", GeneralInfoView.as_view(), name="general-info"),
+    path("general-info", GeneralInfoView.as_view(), name="address"),
+    path("policy", GeneralInfoView.as_view(), name="policy"),
+    path("news-letter", GeneralInfoView.as_view(), name="news-letter")
 ]
