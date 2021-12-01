113c350cfe16fd58ffe18cc0956bbd86f37ddb57
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-01 10:25:21 +0530

    added fields for user

--- user/models.py
+++ user/models.py
@@ -5,9 +5,24 @@ from django.db import models
 class User(AbstractUser):
     image = models.URLField(blank=True, null=True, max_length=255)
 
-    middle_name = models.charField(max_length=True)
+    middle_name = models.CharField(max_length=True)
     gst_number = models.CharField(required=True, max_length=16)
     phone_number = models.CharField(required=True, max_length=16)
+
+    user_type = models.CharField(max_length=20)
+    package_type = models.CharField(max_length=10)
+    package_expiry = models.DateTimeField(auto_now=True)
+    free_ads_remaining = models.IntegerField(default=5)
+    featured_ads_remaining = models.IntegerField(default=0)
+    bumpUp_ads_remaining = models.IntegerField(default=0)
+    allowed_video_links = models.CharField(max_length=40)
+    allowed_tags = models.CharField(max_length=20)
+    allowed_bidding = models.IntegerField(default=0)
+    allowed_images = models.CharField(default=5)
+    categories = models.CharField(max_length=40)
+    package_remaining_days = models.IntegerField(default=30)
+    posts = models.CharField(max_length=60)
+
 
     company_name = models.CharField(required=True, max_length=50)
     company_type = models.CharField(required=True, max_length=30)
