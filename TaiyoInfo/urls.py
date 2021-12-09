527b3cdbe7f2e58f3b196121d54a632fbdc64f4c
savagecarol <karthiksharma1411@gmail.com>
2021-12-10 00:35:24 +0530

    contact and product added

--- TaiyoInfo/urls.py
+++ TaiyoInfo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from TaiyoInfo.views import GeneralInfoView, AddressView, PolicyView, NewsLetterView , ContactView , SubscriptionView
+from TaiyoInfo.views import GeneralInfoView, AddressView, PolicyView, NewsLetterView , ContactView , SubscriptionView , CategoryView , ProductView
 
 urlpatterns = [
     path("general-info", GeneralInfoView.as_view(), name="general-info"),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("policy", PolicyView.as_view(), name="policy"),
     path("news-letter", NewsLetterView.as_view(), name="news-letter"),
     path("subscription", SubscriptionView.as_view(), name="subscription"),
-    path("contact", ContactView.as_view(), name="contact")
+    path("contact", ContactView.as_view(), name="contact"),
+    path("category", CategoryView.as_view(), name="category"),
+    path("product", ProductView.as_view(), name="product")
 ]
