7bd6757aeb2c630667f312e625e2c8896079ee4a
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-16 02:41:53 +0530

    urls

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("recharge/", recharge, name="recharge"),
-    path("billing/", billing, name="billing"),
-    path("success-payment/", success_payment, name="success-payment"),
+    path("billing/", GetBillingUserData.as_view(), name="billing"),
+    path("success-payment/", SuccessPayment.as_view(), name="success-payment"),
 ]
