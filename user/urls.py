8d1397a8094ac552fb17c9d82d5bb1ac4b1153b6
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-21 01:36:23 +0530

    user urls deleted

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,6 @@ from user.views import *
 
 urlpatterns = [
     path('user', UserProfileView.as_view(), name="user"),
-    path('user/<int:id>', UserChanges.as_view(), name="user edit view"),
     path('register', RegisterAPI.as_view(), name="register"),
     path('login', LoginAPIView.as_view(), name="login"),
     path('change-password', ChangePasswordView.as_view(), name='change-password'),
