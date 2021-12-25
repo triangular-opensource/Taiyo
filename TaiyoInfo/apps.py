3790db6f5c79c3e0069c1452b8ab66af717613ba
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-25 19:21:31 +0530

    ready function commented

--- TaiyoInfo/apps.py
+++ TaiyoInfo/apps.py
@@ -30,6 +30,6 @@ class TaiyoinfoConfig(AppConfig):
     name = 'TaiyoInfo'
     verbose_name ="Company Information"
 
-    def ready(self):
-        from .scheduler import scheduler
-        scheduler.start()
+    # def ready(self):
+    #     from .scheduler import scheduler
+    #     scheduler.start()
