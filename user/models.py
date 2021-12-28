a1f790dd15dda432614405211cd6bbd8fe3be78f
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-29 01:50:10 +0530

    middle name issue fixed

--- user/models.py
+++ user/models.py
@@ -9,7 +9,7 @@ from services.validators import validate_gst_number, validate_phone_number, vali
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30, blank=True, null=True)
+    middle_name = models.CharField(max_length=30, default="", blank=True, null=True)
     last_name = models.CharField(max_length=30)
     image = models.URLField(blank=True, null=True, max_length=255)
     gst_number = models.CharField(max_length=15, validators=[validate_gst_number])
