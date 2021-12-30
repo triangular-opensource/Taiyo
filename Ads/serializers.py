10f67d192aa56eab7a63b1176f0eb5703ed3f20b
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-30 14:06:15 +0530

    ads serializer changes

--- Ads/serializers.py
+++ Ads/serializers.py
@@ -21,10 +21,60 @@ class AdvertisementViewSerializer(serializers.ModelSerializer):
 class AdvertisementSerializer(serializers.ModelSerializer):
     image_1 = serializers.FileField(required=False)
     product = ForeignKeyField(queryset=Product.objects, filter_by="name")
+    selected_bid = ForeignKeyField(queryset=Bid.objects, filter_by="amount")
+    selected_bid_amount = serializers.CharField(source="selected_bid.amount")
+    user__first_name = serializers.CharField(source="selected_bid.user.first_name")
+    user__middle_name = serializers.CharField(source="selected_bid.user.middle_name")
+    user__last_name = serializers.CharField(source="selected_bid.user.last_name")
+    user__email = serializers.CharField(source="selected_bid.user.email")
+    user__phone_number = serializers.CharField(source="selected_bid.user.phone_number")
     class Meta:
         model = Advertisement
-        fields = "__all__"
-        read_only_field = "image_1"
+        fields = [
+            "id",
+            "product",
+            "buy_or_sell",
+            "excel_file_link",
+            "pdf_file_link",
+            "image_1",
+            "image_1_link",
+            "image_2_link",
+            "image_3_link",
+            "image_4_link",
+            "quality",
+            "thickness",
+            "width",
+            "length",
+            "grade_or_spec",
+            "temper",
+            "specification_number",
+            "quantity",
+            "coating_in_gsm",
+            "product_description",
+            "bidding",
+            "bidding_close_date",
+            "selected_bid",
+            "selected_bid_amount", 
+            "author_name",
+            "author_mobile_number",
+            "author_country",
+            "author_business_address",
+            "user",
+            "timestamp",
+            "user__first_name",
+            "user__middle_name",
+            "user__last_name",
+            "user__email",
+            "user__phone_number",
+        ]
+        read_only_field = [
+            "image_1",
+            "user__first_name",
+            "user__middle_name",
+            "user__last_name",
+            "user__email",
+            "user__phone_number",
+        ]
 
 
 class BidSerializer(serializers.ModelSerializer):
