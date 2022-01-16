18eb0f94d3feb61730ca5bc00aa858cf1a189cee
savagecarol <karthiksharma1411@gmail.com>
2022-01-16 12:46:24 +0530

    email edited

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -181,7 +181,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "taiyo.apex@gmail.com"
-EMAIL_HOST_PASSWORD = "Gj@19bg9846"
+EMAIL_HOST_PASSWORD = "Testing@321"
 
 FRONTEND_URL = "https://taiyo-768b4.web.app"
 # FRONTEND_URL = "http://localhost:3000"
