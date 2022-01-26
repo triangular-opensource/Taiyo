2d9687e2969947ff7c5ec5149ddbb10baa10d6e3
savagecarol <karthiksharma1411@gmail.com>
2022-01-26 13:02:04 +0530

    change it

--- features/migrations/0004_auto_20220126_1245.py
+++ features/migrations/0004_auto_20220126_1245.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='product',
             name='sub_category',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to='features.subcategory'),
+            field=models.ForeignKey(default= None , on_delete=django.db.models.deletion.CASCADE, to='features.subcategory'),
             preserve_default=False,
         ),
     ]
