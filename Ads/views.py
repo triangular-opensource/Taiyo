f51fd104e73389ae6d8cd3b9ad78d53125910940
Alankar Saxena <saxenaalankar42@gmail.com>
2022-02-06 21:57:30 +0530

    search api

--- Ads/views.py
+++ Ads/views.py
@@ -37,19 +37,12 @@ class AdvertismentView(generics.RetrieveAPIView):
             serializer = self.get_serializer(Advertisement.objects.filter(user = request.user).order_by("-id"), many=True)
         elif case == 11: #Pending for approval ads
             serializer = self.get_serializer(Advertisement.objects.filter(visible = False, user=request.user).order_by("-id"), many=True)
-
-
         elif case == 12: #search
             query = request.GET.get['query']
-            serializer1 = self.get_serializer(Advertisement.objects.filter(visible = True , product__name__icontain = query ).order_by("-id"), many=True)
-            serializer2 = self.get_serializer(Advertisement.objects.filter(visible=True, product__sub_category__name__icontain=query ).order_by("-id"), many=True)
-            serializer3 = self.get_serializer(Advertisement.objects.filter(visible=True, product__sub_category__category__name__icontain=query ).order_by("-id"), many=True
-
-
-
-
-
-
+            serializer1 = self.get_serializer(Advertisement.objects.filter(visible = True , product__name__icontains = query ).order_by("-id"), many=True)
+            serializer2 = self.get_serializer(Advertisement.objects.filter(visible=True, product__subcategory__name__icontains=query ).order_by("-id"), many=True)
+            serializer3 = self.get_serializer(Advertisement.objects.filter(visible=True, product__subcategory__category__name__icontains=query ).order_by("-id"), many=True)
+            serializer = serializer1 | serializer2 | serializer3
         else:
             serializer = self.get_serializer(Advertisement.objects.all().order_by("-id"), many=True)
         return success_response(serializer.data)
