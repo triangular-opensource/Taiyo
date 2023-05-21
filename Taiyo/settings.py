6fdaa90253ddfbfd1393c1d9c7f07aec1174d589
kartikeya sharma <karthiksharma1411@gmail.com>
2023-05-21 11:03:47 +0530

    sd

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -164,9 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -175,7 +173,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
 
 DEFAULT_FROM_EMAIL = 'taiyo.apex@gmail.com'
 SERVER_EMAIL = 'taiyo.apex@gmail.com'
