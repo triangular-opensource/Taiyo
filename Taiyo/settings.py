4f506905ce6cc7ff68f82749e00d9f7ea4e05665
kartikeya sharma <karthiksharma1411@gmail.com>
2023-05-21 10:51:57 +0530

    more changes in db

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -110,8 +110,12 @@ WSGI_APPLICATION = 'Taiyo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Taiyo',
+        'USER': 'Manoj',
+        'PASSWORD': 'ManojMishra123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
