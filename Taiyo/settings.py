97e76593485860d82c86c75e14686785c376fdd5
kartikeya sharma <karthiksharma1411@gmail.com>
2023-05-21 11:01:15 +0530

    changes in db

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '12222222'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
