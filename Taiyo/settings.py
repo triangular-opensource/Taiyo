92209518e1c3f37630d02ae5f341917884cfa681
kartikeya sharma <karthiksharma1411@gmail.com>
2024-02-20 14:12:48 +0530

    changing local db

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -110,7 +110,14 @@ WSGI_APPLICATION = 'Taiyo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 # Password validation
