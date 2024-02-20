8511867cc25d2f5a99a93ef93cd2f2699c474ecb
kartikeya sharma <karthiksharma1411@gmail.com>
2024-02-20 14:28:00 +0530

    changing local db

--- Taiyo/settings.py
+++ Taiyo/settings.py
@@ -83,7 +83,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
 ]
 
 ROOT_URLCONF = 'Taiyo.urls'
